598b765d715ad861ff9e482fdcc5ec46543620d1
David L <120467347+FuzionDragon@users.noreply.github.com>
2024-12-25 19:48:38 +0000

    Working state with basic text editor editing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use clap::{builder::TypedValueParser, command, error::Result, Parser};
-use rusqlite::{Connection, Result};
+use clap::{command, Parser, Subcommand};
+use rusqlite::{Connection};
+use edit::{self, edit_file};
 use std::{
   env::var,
   fs::File,
@@ -10,25 +11,62 @@ use std::{
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-  /// Edit tasks in default text editor $EDITOR
-  #[arg(short, long, default_missing_value="always")]
-  edit: String,  
+  #[arg(short, long, default_value="tasks.txt")]
+  path: String,
 
-  /// Lists all tasks in a given tracker
-  #[arg(short, long)]
-  list: String,  
-
-  /// Clears all tasks in a given tracker
-  #[arg(short, long)]
-  clear: String,  
-
-  /// Queries a specific task based on ID
-  #[arg(short, long)]
-  query: String,  
+  #[command(subcommand)]
+  command: Option<Commands>,
 }
 
-fn main() -> Result<()> {
-  let args = Args::parse();
+#[derive(Subcommand)]
+enum Commands {
+  /// Edit tasks in default text editor $EDITOR
+  Edit {
+    file_path: String,
+  },
+  /// Lists all tasks in a given tracker
+  List {
+    file_path: String,
+  },
+  /// Clears all tasks in a given tracker
+  Clear {
+    file_path: String,
+  },
+  /// Queries a specific task based on ID
+  Query {
+    id: String,
+  },
+}
 
-  Ok(());
+fn main() {
+  let args = Args::parse();
+  let path = &args.path;
+
+  match &args.command {
+    Some(Commands::Edit { file_path }) => {
+      drop(file_path);
+      edit_tasks(file_path);
+    },
+
+    Some(Commands::List { file_path }) => {
+      println!("List");
+    },
+
+    Some(Commands::Clear { file_path }) => {
+      println!("Clear");
+    },
+
+    Some(Commands::Query { id }) => {
+      println!("Query");
+    },
+
+    None => {
+      edit_tasks(path);
+    },
+  }
+}
+
+fn edit_tasks(path: &String) {
+  println!("Editing: {}", &path);
+  let edited = edit::edit_file(path).expect("Unable to edit file");
 }
